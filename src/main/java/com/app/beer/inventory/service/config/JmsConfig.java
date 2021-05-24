19191f029f1e60c433882cb7d190f822ad01a839
t0k02w6 <tanaya.karmakar@walmartlabs.com>
2021-05-24 09:02:58 +0530

    added jms configuration

---
+++ src/main/java/com/app/beer/inventory/service/config/JmsConfig.java
@@ -0,0 +1,20 @@
+package com.app.beer.inventory.service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+/**
+ * @author t0k02w6 on 24/05/21
+ * @project mssc-beer-inventory-service
+ */
+public class JmsConfig {
+    @Bean
+    public MessageConverter jacksonJmsMessageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+}
