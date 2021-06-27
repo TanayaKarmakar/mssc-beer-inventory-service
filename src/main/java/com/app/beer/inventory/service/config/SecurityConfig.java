d4eaa307fdadbaaf3efe29da2df9158b94dd2fcd
t0k02w6 <tanaya.karmakar@walmartlabs.com>
2021-06-27 18:08:07 +0530

    added http basic auth for beer inventory service

---
+++ src/main/java/com/app/beer/inventory/service/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.app.beer.inventory.service.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author t0k02w6 on 27/06/21
+ * @project mssc-beer-inventory-service
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+}
